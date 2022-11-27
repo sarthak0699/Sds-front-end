3fce4bfef5fb3ac1fe76e1bc5915a59e07bdf3a0
Sanjana Mukherjee (Student) <smukhe52@sundevils.asu.edu>
2022-11-27 14:45:27 -0700

    dataset browse added

--- sds-final-project/src/App2.jsx
+++ sds-final-project/src/App2.jsx
@@ -21,6 +21,9 @@ import SR from './SR';
 
 // var done = true
 function App2() {
+
+  const [fileName,setFileName]= useState("data\\simulated_trajectories.json")
+  const [fileOptions,setFileOptions]= useState([]);
   const [ind,setInd] = useState(0)
   const [trips,setTrips] = useState([{path:[],timestamps:[]}])
   var formValues = {lonMin:"",lonMax:"",timeMin:"",timeMax:"",latMin:"",latMax:"",trajectory_id:0,neighbors:0}
@@ -52,67 +55,26 @@ function App2() {
 
  var baseUrl = "http://localhost:9000"
 
+ useEffect(()=>{
+  //get file names for the dataset
+  axios.get(baseUrl+"/files").then(res=>{
+    // console.log("filenames",res.data);
+    setFileOptions(res.data)
+  })
+
+ },[])
 
  useEffect(()=>{
   // get trajectories
   console.log("in App2------")
 
-  axios.get(baseUrl+"/trajectories").then(val =>{
+  axios.get(baseUrl+"/trajectories?filepath="+fileName).then(val =>{
     data = val.data
     console.log("data in trajectories - ", data)
     
-    setTrips(getPathAndTime(data))})
-
-    // setTripLayer(new PathLayer({
-    //   id: 'TripsLayer',
-    //   data : trips,
-      
-    //   /* props from TripsLayer class */
-    //   // fadeTrail: true,
-    //   /* props inherited from PathLayer class */
-      
-    //   // billboard: false,
-    //   capRounded: true,
-    //   getColor: [253, 128, 93],
-    //   getPath: d => d.path,
-    //   // getWidth: 1,
-    //   jointRounded: true,
-    //   // miterLimit: 4,
-    //   // rounded: true,
-    //   // widthMaxPixels: Number.MAX_SAFE_INTEGER,
-    //   widthMinPixels: 1,
-    //   widthScale: 1,
-    //   // widthUnits: 'meters',
-      
-    //   /* props inherited from Layer class */
-      
-    //   // autoHighlight: false,
-    //   // coordinateOrigin: [0, 0, 0],
-    //   // coordinateSystem: COORDINATE_SYSTEM.LNGLAT,
-    //   // highlightColor: [0, 0, 128, 128],
-    //   // modelMatrix: null,
-    //   opacity: 0.8,
-    //   // pickable: false,
-    //   // visible: true,
-    //   // wrapLongitude: false,
-    // }))
+    setTrips(getPathAndTime(data))}) 
   
-
-  //get knn results
-  // axios.post(baseUrl+'/knn',{
-  //   "trajectory_id": 0,
-  //   "neighbors": 5
-  // })
-  // .then((response)=> {
-  //     console.log("knn result-");
-  //     console.log(response.data);
-  //   })
-  //   .catch((error)=> {
-  //     console.log("knn error-");
-  //     console.log(error);
-  //   });
-
-},[])
+},[fileName])
 
 const tripLayer0 = new PathLayer({
   id: Math.random(10),
@@ -171,7 +133,7 @@ const tripLayer0 = new PathLayer({
 
   const onSpatialClick = () =>{
     console.log(formValues)
-    axios.post(spatialUrl,data = {...formValues}).then(res =>{
+    axios.post(spatialUrl+"?filepath="+fileName,data = {...formValues}).then(res =>{
       setTrips(getPathAndTime(res.data))
 
     }).catch(e =>{
@@ -183,7 +145,7 @@ const tripLayer0 = new PathLayer({
 
     var traj_ids=[]
     console.log("KNN clicked---", formValues);
-    axios.post(baseUrl+'/knn',data={
+    axios.post(baseUrl+'/knn?filepath='+fileName,data={
         "trajectory_id": parseInt(formValues.trajectory_id),
         "neighbors": parseInt(formValues.neighbors)
       })
@@ -193,7 +155,7 @@ const tripLayer0 = new PathLayer({
           response.data.forEach((s)=>
           traj_ids.push(s.trajectory_id))
 
-          axios.get(baseUrl+"/trajectories").then(val =>{
+          axios.get(baseUrl+"/trajectories?filepath="+fileName).then(val =>{
             data = val.data
             // console.log("data in trajectories for knn - ", data)
             // console.log("traj ids -",traj_ids)
@@ -216,6 +178,7 @@ const tripLayer0 = new PathLayer({
 
    return (
     <>
+    {console.log("fileOptions - >",fileOptions)}
     <div className='app2'>
     <div>
       <Navbar bg="dark" expand="lg" className='navBar'>
@@ -228,8 +191,17 @@ const tripLayer0 = new PathLayer({
               <Nav.Link className='api' onClick={onSpatialClick}>Spatio-Temporal Query</Nav.Link>
               <Nav.Link className='api' onClick={onSpatialClick}>Spatial Query</Nav.Link>
               <Nav.Link className='api' onClick={onKnnClick}>Knn</Nav.Link>
+              
             </Nav>
           </Navbar.Collapse>
+          <div>
+            <label style={{color: "white"}} className='api'>Select a dataset - </label>
+            <select name="fileName" id="fileName" onChange={(e)=>{console.log("selected filename-",e.target.value);setFileName(e.target.value)}}>
+              {fileOptions.map((f)=>{
+                return <option value={f} >{f}</option>
+              })}
+            </select>
+          </div>
         </Container>
       </Navbar>
     </div>
